9e4acd9df239f129a281c2291dfe02c40cd0a8e6
lakotelman <la.n.kot@pm.me>
2024-07-13 19:57:26 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1> Roll For Breakfast</h1>
+      <button @click=""><p>Roll</p></button>
+
+    
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
